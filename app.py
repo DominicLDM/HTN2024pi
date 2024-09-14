6263ac46e1d2acfda771b929623a204da7778ae2
Dominic Lemoine de Martigny <dodolem0306@gmail.com>
2024-09-14 15:52:18 -0400

    symphonic api test

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, Response, send_from_directory, render_template
 import cv2
 import os
+import requests
+import io
+import time
 
 app = Flask(__name__)
 
@@ -8,12 +11,21 @@ app = Flask(__name__)
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
 # Function to generate frames
 def generate_frames():
+    clip_number = 0
+    start_time = time.time()
+    out = cv2.VideoWriter(f'output_{clip_number}.mp4', fourcc, 20.0, (frame_width, frame_height))
     while True:
         # Capture frame-by-frame from the camera
         ret, frame = cap.read()
         if not ret:
+            break
+        if cv2.waitKey(1) == ord('q'):
             break
 
         # Convert frame to grayscale for face detection
@@ -25,6 +37,16 @@ def generate_frames():
         # Draw rectangles around detected faces
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            
+        out.write(frame)
+        
+        if time.time() - start_time >= 20:
+            out.release()
+            transcribed_text = transcribe(f'output_{clip_number}.mp4')
+            print(f'Transcription for clip {clip_number}: {transcribed_text}')  
+            clip_number += 1
+            start_time = time.time()
+            out = cv2.VideoWriter(f'output_{clip_number}.mp4', fourcc, 20.0, (frame_width, frame_height))
 
         # Encode the frame in JPEG format
         _, buffer = cv2.imencode('.jpg', frame)
@@ -32,7 +54,20 @@ def generate_frames():
 
         # Yield the frame to create a stream
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+    out.release()
+    cap.release()
+    cv2.destroyAllWindows
+
+def transcribe(file_path):
+    url = "https://symphoniclabs--symphonet-vsr-modal-htn-model-upload-static-htn.modal.run"
+
+    with open(file_path, 'rb') as video_file:
+        video = io.BytesIO(video_file.read())
+
+    response = requests.post(url, files={'video': (file_path, video, 'video/mp4')})
+    
+    return response.text
 
 # Route to stream the video
 @app.route('/video_feed')
